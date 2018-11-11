0efa01db31912a30338872545bce73549e001a97
Konservator-mn <fryb08179@gmail.com>
2018-11-11 22:39:27 +0200

    almoste comlete 8 homework

--- homework8/hw8.js
+++ homework8/hw8.js
@@ -1,3 +1,36 @@
-function filterCollection(arr, keyWords, useAllWords) {
+function filterCollection(arr=[], keyWords="", useAllWords=true) {
+    let keyWordsCollection = keyWords.split(" ");
+    
+    keyWordsCollection.correspondsTo = function (str) {
+        return !this.some(
+            useAllWords?
+            (keyWord)=>{
+                return !str.includes(keyWord);
+            }:
+            (keyWord)=>{
+                return str.includes(keyWord);
+            }
+        );
+    };
+
+    let paths = [];
+    for (let argNum = 3; argNum<arguments.length; ++argNum)
+        paths.push(arguments[argNum]);
+    
+    paths.getValByPath = function (obj) {
+        let depth = path.length;
+        return deepper(obj);
+        function deepper(obj) {
+            let current = obj[paths[paths.length-depth]];
+            return (depth--)?deepper(current):current;
+        }
+        
+    };
+
+    function searchIn (data) {
+        if (Array.isArray(data)){
+            
+        }
+    }
 
 }
